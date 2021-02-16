a5100b9f73948c63a998e7fc77db6621c7b5b4b4
garcanm8852 <a20garcanm8852@ies-mardecadiz.com>
2021-02-16 11:44:01 +0100

    Animaciones y Video Realizado

--- JS/JQuery.js
+++ JS/JQuery.js
@@ -1,5 +1,16 @@
-        $(window).resize(function() {
-            $("#Resolucion").text();
-            $("#Resolucion").text($(window).width() + "");
+         $(window).resize(function() {
+             $("#Resolucion").text();
+             $("#Resolucion").text($(window).width() + "");
 
-        });
+         });
+
+         $("#AccionadorFundido").click(function() {
+             $("#Animacion").animate({
+                 opacity: 'toggle',
+             });
+         });
+         $("#AccionadorPersiana").click(function() {
+             $("#Animacion").animate({
+                 height: 'toggle',
+             });
+         });
